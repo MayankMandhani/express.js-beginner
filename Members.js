a1712e7a37f84eb4642a16c8d48418e337d7bb44
MayankMandhani <43675024+MayankMandhani@users.noreply.github.com>
2020-06-26 19:49:41 +0530

    upload

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members=[
+    {
+        id:1,
+        name: 'k',
+        email:'k@m.com',
+        status:'active'
+    },
+    {
+        id:2,
+        name: 'l',
+        email:'l@m.com',
+        status:'inactive'
+    },
+    {
+        id:3,
+        name: 'm',
+        email:'m@m.com',
+        status:'active'
+    }
+];
+
+module.exports=members;
